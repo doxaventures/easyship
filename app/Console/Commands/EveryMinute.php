61a169bd641d385161848f6c85ff68bdaebbf1b6
Haider112 <haiderzeeshan112@gmail.com>
2019-11-04 00:40:59 -0800

    'aaa'

---
+++ app/Console/Commands/EveryMinute.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\ShipmentController;
+use Illuminate\Console\Command;
+
+class EveryMinute extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'minute:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update shipping Status every minute';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $update = new ShipmentController();
+        $update->get_shipment();
+        $this->info('Shipment watching regularly');
+    }
+}
