11e0b1807ce35b931f6f7f44da8da6446946cbc5
Haider112 <haiderzeeshan112@gmail.com>
2019-11-03 23:52:35 -0800

    'aaa'

--- app/Http/Controllers/ShipmentController.php
+++ app/Http/Controllers/ShipmentController.php
@@ -15,7 +15,7 @@ class ShipmentController extends Controller
     public function get_shipment(){
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, "https://api.easyship.com/shipment/v1/shipments/{easyship_shipment_id}?format=PNG&label=4X6&commercial_invoice=4X6&packip_slip=4X6");
+        curl_setopt($ch, CURLOPT_URL, "https://api.easyship.com/shipment/v1/shipments?easyship_shipment_id=&platform_order_number=&shipment_state=&pickup_state=&delivery_state=&label_state=&created_at_from=&created_at_to=&confirmed_at_from=&confirmed_at_to=&per_page=&page=");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
