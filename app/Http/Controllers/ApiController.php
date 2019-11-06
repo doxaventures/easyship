7744f9a3bbc737f25d82abda6acc3f0589abe273
Haider112 <haiderzeeshan112@gmail.com>
2019-11-06 04:51:51 -0800

    'aaa'

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -60,7 +60,6 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 $response = curl_exec($ch);
 curl_close($ch);
 $res=json_decode($response);
-dd($res);
         return response()->json(['data'=>$response]);
 
     }
