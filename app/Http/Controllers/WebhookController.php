63bb100c0568e52f01b7c9794aeccd46989f663b
Haider112 <haiderzeeshan112@gmail.com>
2019-11-08 02:04:35 -0800

    'aaa'

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -36,7 +36,7 @@ class WebhookController extends Controller
     public function webhook_order_create(Request $request)
     {
         $json = file_get_contents('php://input');
-    $order = json_encode($json);
+    $order = json_decode($json);
 
         $test=new test();
         $test->data=$order['id'];
