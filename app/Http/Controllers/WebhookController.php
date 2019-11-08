17f3de615126b4305cb2a29949342264d9cceeda
Haider112 <haiderzeeshan112@gmail.com>
2019-11-08 01:07:32 -0800

    'aaa'

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -39,7 +39,7 @@ class WebhookController extends Controller
     $order = json_encode($json, true);
 //    $order = $order;
    $test=new test();
-   $test->data=$json[0];
+   $test->data=$order[0];
    $test->save();
 //        $orders->get_orders();
     }
